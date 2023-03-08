374c9626f865ad0bb7d8758b047c5e52f11c8a9d
elya2305 <elinafromua@gmail.com>
2023-03-08 14:28:26 +0200

    calculator was added

--- src/main/java/ua/robert/lohkamp/springcourse/controllers/FirstController.java
+++ src/main/java/ua/robert/lohkamp/springcourse/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ua.robert.lohkamp.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -8,31 +9,32 @@ import java.util.Map;
 
 @Controller
 public class FirstController {
+
     @GetMapping("/hello")
-    public String helloPage(@RequestParam Map<String, String> params) {
-//        String name = request.getParameter("name");
-        System.out.println("Hello, " + params);
-        System.out.println("Hello, " + params.get("name"));
+    public String helloPage2(@RequestParam(value = "name", required = false)  String name, Model model) {
+        model.addAttribute("message", "Hello, " + name);
 
         return "first/hello";
     }
 
-    @GetMapping("/hello2")
-    public String helloPage2(@RequestParam("name") String name) {
-//        String name = request.getParameter("name");
-        System.out.println("Hello, " + name);
-
-        return "first/hello";
+    @GetMapping("/calculator")
+    public String calculatorPage(@RequestParam(value = "a", required = false) Integer a,
+                                 @RequestParam(value = "b", required = false) Integer b,
+                                 @RequestParam(value = "operation", required = false) String operation,
+                                 Model model) {
+        int numberA = a;
+        int numberB = b;
+        int result = switch (operation) {
+            case "+" -> numberA + numberB;
+            case "-" -> numberA - numberB;
+            case "*" -> numberA * numberB;
+            case "/" -> numberA / numberB;
+            default -> 220;
+        };
+        String message = a + operation + b + " = " + result;
+        model.addAttribute("mesage", message);
+        return "first/calculator";
     }
-
-    @GetMapping("/hello3")
-    public String helloPage3(@RequestParam(value = "name", required = false) String name) {
-//        String name = request.getParameter("name");
-        System.out.println("Hello, " + name);
-
-        return "first/hello";
-    }
-
 
     @GetMapping("/goodbye")
     public String goodByePage() {
