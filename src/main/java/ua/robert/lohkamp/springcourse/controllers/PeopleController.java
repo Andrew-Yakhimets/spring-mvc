79e36a2bb5a77c3ad5f5bb7ce7ba16e9408e3b57
Andrii Yakhimets <andriy.yahimets@gmail.com>
2023-03-09 16:45:37 +0200

    new changes

--- src/main/java/ua/robert/lohkamp/springcourse/controllers/PeopleController.java
+++ src/main/java/ua/robert/lohkamp/springcourse/controllers/PeopleController.java
@@ -1,2 +1,47 @@
-package ua.robert.lohkamp.springcourse.controllers;public class PeopleController {
+package ua.robert.lohkamp.springcourse.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ua.robert.lohkamp.springcourse.dao.PersonDAO;
+import ua.robert.lohkamp.springcourse.models.Person;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public PeopleController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+
+    @GetMapping()
+    public String index(Model model) {
+        //Get all people from DAO and transfer to the html page
+        model.addAttribute("people", personDAO.index());
+        return "people/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        //Show person by index from DAO and transfer it to the html page
+        model.addAttribute("person", personDAO.show(id));
+        return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(Model model) {
+        model.addAttribute("person", new Person());
+        return "people/new";
+    }
+
+    @PostMapping
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
+    }
 }
--- src/main/java/ua/robert/lohkamp/springcourse/dao/PersonDAO.java
+++ src/main/java/ua/robert/lohkamp/springcourse/dao/PersonDAO.java
@@ -1,2 +1,38 @@
-package ua.robert.lohkamp.springcourse.dao;public class PersonDAO {
+package ua.robert.lohkamp.springcourse.dao;
+
+import org.springframework.stereotype.Component;
+import ua.robert.lohkamp.springcourse.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> personList;
+
+    {
+        personList = new ArrayList<>();
+
+        personList.add(new Person(++PEOPLE_COUNT, "Andrew"));
+        personList.add(new Person(++PEOPLE_COUNT, "Tom"));
+        personList.add(new Person(++PEOPLE_COUNT, "Bob"));
+        personList.add(new Person(++PEOPLE_COUNT, "Alice"));
+    }
+
+    public List<Person> index() {
+        return personList;
+    }
+
+    public Person show(int id) {
+        return personList.stream()
+                .filter(person -> person.getId() == id)
+                .findAny()
+                .orElse(null);
+    }
+
+    public void save(Person person) {
+        person.setId(++PEOPLE_COUNT);
+        personList.add(person);
+    }
 }
